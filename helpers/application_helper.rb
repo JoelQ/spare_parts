b10ed9bec7cef6fbb9c103a3be3f9f7afa4ca673
Joël Quenneville <joelq@thoughtbot.com>
2013-08-02 11:38:11 -0400

    Compile SCSS styles
    * Take advantage of SCSS to wrap the styles with a 'namespace'

--- helpers/application_helper.rb
+++ helpers/application_helper.rb
@@ -1,5 +1,13 @@
+require 'sass'
+
 module ApplicationHelper
   def anchor_from_title(title)
     title.downcase.gsub(' ', '_')
+  end
+
+  def wrap_css_with_class(css, class_name)
+    wrapped_css = ".#{class_name} {#{css}}"
+    engine = Sass::Engine.new(wrapped_css, syntax: :scss)
+    engine.render
   end
 end
--- spec/application_helper_spec.rb
+++ spec/application_helper_spec.rb
@@ -1,11 +1,22 @@
 require_relative '../helpers/application_helper'
-describe ApplicationHelper, '#anchor_from_title' do
-  class HelperTest
-    include ApplicationHelper
-  end
 
+class HelperTest
+  include ApplicationHelper
+end
+
+describe ApplicationHelper, '#anchor_from_title' do
   it 'converts a title to snake case' do
     h = HelperTest.new
     expect(h.anchor_from_title('A Cool title')).to eq 'a_cool_title'
+  end
+end
+
+describe ApplicationHelper, '#wrap_css_with_class' do
+  it 'wraps each rule with the given class name' do
+    css = "div {\n  background-color: red; }\n p {\n  color: yellow; }\n"
+    wrapped_css = ".wrapper div {\n  background-color: red; }\n.wrapper p {\n  color: yellow; }\n"
+    h = HelperTest.new
+
+    expect(h.wrap_css_with_class(css, 'wrapper')).to eq wrapped_css
   end
 end
